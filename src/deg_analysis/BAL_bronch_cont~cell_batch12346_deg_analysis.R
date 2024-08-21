1afea95a1593153e64bef264bd8b4bd880ce73b8
yjkimmdphd <29801275+yjkimmdphd@users.noreply.github.com>
2024-08-21 15:43:40 -0400

    update the custom function and how count table and phenotype tables are called in

---
+++ src/deg_analysis/BAL_bronch_cont~cell_batch12346_deg_analysis.R
@@ -0,0 +1,300 @@
+## 
+# Bronchial RNAseq analysis with new samples:
+# additional samples run in the batch 6
+# alignment based on new reference genome GRCh38
+# phenotype table was updated. loading was simplified
+##
+
+library(tidyverse)
+library(DESeq2)
+library(limma)
+library(edgeR)
+
+# load cell count table
+countdata<-file.path("./resources/raw_data/MS_asthma/MS_asthma.batch12346.GRCh38.geneID_readcount.all_samples.QCed_final.txt")
+counts<-if(file.exists(countdata)){read.delim(countdata, check.names = FALSE)}
+genes<-counts[,"SampleID"]
+
+# select bronchial samples 
+bronch.samples<-grepl("^B",colnames(counts))
+bronch.counts<-counts[,bronch.samples]
+rownames(bronch.counts)<-genes
+head(bronch.counts)
+counts.ID<-colnames(bronch.counts)
+
+################################
+## load phenotype and batch data
+################################
+
+# make vectors of variables for later use as an input for function 'run_deseq2_DEG_analysis'
+
+## source.cell.log is log-transformed, scaled, and centered cell counts. 
+## source.cell is the original BAL/blood cell counts 
+source.cell.log<-c(
+  "BAL_eos_ct_log",
+  "BAL_eos_p_log",
+  "BAL_neut_ct_log",
+  "BAL_neut_p_log",
+  "BAL_wbc_log",
+  "blood_eos_log",
+  "blood_eos_p_log",
+  "blood_neut_log",
+  "blood_neut_p_log",
+  "blood_wbc_log")
+
+source.cell<-c(
+  "BAL_eos_ct",
+  "BAL_eos_p",
+  "BAL_neut_ct",
+  "BAL_neut_p",
+  "BAL_wbc",
+  "blood_eos",
+  "blood_eos_p",
+  "blood_neut",
+  "blood_neut_p",
+  "blood_wbc")
+
+# load biomarker phenotype file saved in  'phenotype'
+phenotype<-file.path("./resources/processed_data/scaled_phenotype_studyID_asthmaPhenotype_batch_cellCount_20240731.csv")
+phenotype<-if(file.exists(phenotype)){read.csv(phenotype, row.names = NULL)}
+
+
+#####################################################################################
+## subset phenotype data for which the samples exist for bronchial RNAseq experiments   
+#####################################################################################
+bexist<-phenotype$SampleID%in%counts.ID # find which subjects s/p BAL and had bronchial sample RNAseq completed 
+bphen<-phenotype[bexist,]
+
+
+###################################
+# custom functions for DEG analysis
+###################################
+# should load the following fx:
+## filter_low_expressed_genes_method2: Filters low counts genes using TMM normalized lcpm as a cutoff point. Requires 'limma'
+## rowgenes_counttable: changes the row names of the count table with gene names
+## run_deseq2_DEG_analysis: takes countdata,coldata,design,des as variables, and runs DEG on DESeq2
+## get_DEG_results: saves result of DESeq2 output, ordered in padj 
+## generate_DEG_input_summary_table: makes a table of input information
+## generate_DEG_summary_table: makes results summary (i.e., # of DEG for each analysis)
+
+source("./src/function/deg_custom_functions_v2.R")
+
+# all non-NA values. cell count >=0
+pi<-lapply(phen[,source.cell.log],function(data){a<-!is.na(data);return(a)})
+df<-vector("list",length=10) # list of data framese used as an input for deseq2. all cell counts
+names(df)<-paste(source.cell.log,"all",sep="_")
+for(i in seq_along(df)){
+  df[[i]]<-phen[pi[[i]],c("SampleID",source.cell.log[i], "Batch")]
+}
+print(sapply(df,dim)[1,])
+
+# all cell count >0
+pi.pos<-lapply(phen[,source.cell],function(data){a<-which(data>0);return(a)})
+df.pos<-vector("list",length=length(source.cell)) # list of data frames used as an input for deseq2. subset cell count > 0
+names(df.pos)<-paste(source.cell.log,"pos",sep = "_")
+for(i in seq_along(df.pos)){
+  df.pos[[i]]<-phen[pi.pos[[i]],c("SampleID",source.cell.log[i], "Batch")]
+}
+print(sapply(df.pos,dim)[1,])
+# if analyzing only cell counts >0, use df<-df.pos
+
+#################################################################
+# bronchial expression ~ log(cell count>=0) + Batch12346
+#################################################################
+# coldata for DESeq2
+df.input<-df
+
+
+# filtering counts table to remove low expressed genes
+
+## select RNAseq counts
+id<-phen$SampleID
+cols<-colnames(bronch.counts)%in%id
+ct<-bronch.counts[,cols] # First column is actually gene name 
+genes<-bronch.counts$SampleID
+rownames(ct)<-genes
+
+## Filter counts 
+c2<-filter_low_expressed_genes_method2(ct,7)
+
+
+# run the DEG for continuous predictors
+
+deg.design<-paste("~",source.cell.log,"+ Batch")
+print(deg.design)
+ct<-rowgenes_counttable(ct,c2) # low bcounts will be filtered using method 2: use TMM normalized lcpm as a cutoff point
+
+print(deg.design)
+
+# make a list of count table. Each element is a count table with samples for each of the experimental design. 
+count.table<-lapply(df.input,function(df){d<-df; ct<-ct[,colnames(ct)%in%d$SampleID]; return(ct)}) 
+
+dds<-vector("list",length=length(df.input))
+res<-vector("list",length=length(df.input))
+res.sig<-vector("list",length=length(df.input))
+
+names(res)<-deg.design
+names(res.sig)<-deg.design
+
+for(i in seq_along(length(df.input))){
+  dds[[i]]<-run_deseq2_DEG_analysis(count.table[[i]], df.input[[i]], deg.design[i],deg.design[i])
+  res[[i]]<-get_DEG_results(dds[[i]], source.cell.log[i])
+  res.sig[[i]]<-res[[i]][which(res[[i]]$padj<=0.05),]
+  head(res.sig[[i]])
+  
+}
+
+## writing the significant and all results 
+deg.folder<-paste("deg",Sys.Date(),sep="_")
+deg.dir<-file.path("./reports",deg.folder)
+if(!dir.exists(deg.dir)){
+  dir.create(deg.dir)
+}
+
+if(dir.exists(deg.dir)){
+  for(i in 1:length(df)){
+    a<-res.sig[[i]]
+    b<-res[[i]]
+    write.csv(a,row.names=TRUE,file.path(deg.dir,paste("deg","bronch","continuous_allcell+batch12346","res_sig",i,deg.design[[i]],Sys.Date(),".csv",sep="_")))
+    write.csv(b,row.names=TRUE,file.path(deg.dir,paste("deg","bronch","continuous_allcell+batch12346","res_all",i,deg.design[[i]],Sys.Date(),".csv",sep="_")))
+  }
+}
+
+## summarize the data input 
+
+generate_DEG_input_summary_table<-function(original_ct,filtered_ct,dds,res,des){
+  filter_method<-"TMM normalized LCPM cutoff"
+  n_filtered_genes<-paste("analyzed n_genes:", nrow(filtered_ct),",","filtered n_genes:",nrow(original_ct)-nrow(filtered_ct))
+  samples<-sapply(dds, function(d){colData(d)$SampleID%>%paste(collapse = ",")})
+  dds<-paste("dds",1:length(dds),sep="")
+  results<-paste("res",1:length(res),sep="")
+  design<-des
+  df<-data.frame(dds=dds,results=results,design=design,samples=samples,filter_method=filter_method,n_filtered_genes=n_filtered_genes)
+  return(df)
+}
+
+
+if(dir.exists(deg.dir)){
+  a<-generate_DEG_input_summary_table(bronch.counts[,cols],ct,dds,res,deg.design)
+  write.csv(a,row.names=FALSE,file.path(deg.dir,paste("deg","bronch","continuous","analysis_input","cellcount_allcell+Batch12346",Sys.Date(),".csv",sep="_")))
+}
+
+## summary table of the DEG analysis
+generate_DEG_summary_table<-function(results_significant,deg_design,variable){
+  res.sig<-results_significant
+  deg.design<-deg_design
+  var<-variable
+  
+  reslist<-paste("res.sig",1:length(res.sig),sep="")
+  n_sig_deg<-unlist(sapply(res.sig,nrow))
+  design<-deg.design
+  
+  df<-data.frame(type="bronch",results=reslist,n_sig_deg=n_sig_deg,design=design,variable=var, row.names = NULL)
+  return(df)
+}
+
+if(dir.exists(deg.dir)){
+  a<-generate_DEG_summary_table(res.sig,deg.design,source.cell.log)
+  write.csv(a,row.names=FALSE,file.path(deg.dir,paste("dds","bronch","continuous","res_summary","cellcount_allcell+Batch12346",Sys.Date(),".csv",sep="_")))
+}
+
+#################################################################
+# bronchial expression ~ log(cell count>0) + Batch
+#################################################################
+# coldata for DESeq2
+df.input<-df.pos
+
+# filtering counts table to remove low expressed genes
+
+## select RNAseq counts
+id<-phen$SampleID
+cols<-colnames(bronch.counts)%in%id
+ct<-bronch.counts[,cols] # First column is actually gene name 
+genes<-bronch.counts$SampleID
+rownames(ct)<-genes
+
+## Filter counts
+c2<-filter_low_expressed_genes_method2(ct,7)
+
+
+# run the DEG for continuous predictors
+
+deg.design<-paste("~",source.cell.log,"+ Batch") 
+ct<-rowgenes_counttable(ct,c2) # low bcounts will be filtered using method 2: use TMM normalized lcpm as a cutoff point
+
+print(deg.design)
+
+# list of subsetted count table. Each element is a count table with samples for each of the experimental design. 
+count.table<-lapply(df.input,function(df){d<-df; ct<-ct[,colnames(ct)%in%d$SampleID]; return(ct)})
+
+dds<-vector("list",length=10)
+res<-vector("list",length=10)
+res.sig<-vector("list",length=10)
+
+names(res)<-deg.design
+names(res.sig)<-deg.design
+
+for(i in 1:10){
+  dds[[i]]<-run_deseq2_DEG_analysis(count.table[[i]], df.input[[i]], deg.design[i],deg.design[i])
+  res[[i]]<-get_DEG_results(dds[[i]], source.cell.log[i])
+  res.sig[[i]]<-res[[i]][which(res[[i]]$padj<=0.05),]
+  head(res.sig[[i]])
+  
+}
+
+
+
+
+## writing the significant and all results 
+deg.folder<-paste("deg",Sys.Date(),sep="_")
+deg.dir<-file.path("./reports",deg.folder)
+if(!dir.exists(deg.dir)){
+  dir.create(deg.dir)
+}
+
+if(dir.exists(deg.dir)){
+  for(i in 1:length(df)){
+    a<-res.sig[[i]]
+    b<-res[[i]]
+    write.csv(a,row.names=TRUE,file.path(deg.dir,paste("deg","bronch","continuous_poscell+batch12346","res_sig",i,deg.design[[i]],Sys.Date(),".csv",sep="_")))
+    write.csv(b,row.names=TRUE,file.path(deg.dir,paste("deg","bronch","continuous_poscell+batch12346","res_all",i,deg.design[[i]],Sys.Date(),".csv",sep="_")))
+  }
+}
+
+## summarize the data input 
+
+generate_DEG_input_summary_table<-function(original_ct,filtered_ct,dds,res,des){
+  filter_method<-"TMM normalized LCPM cutoff"
+  n_filtered_genes<-paste("analyzed n_genes:", nrow(filtered_ct),",","filtered n_genes:",nrow(original_ct)-nrow(filtered_ct))
+  samples<-sapply(dds, function(d){colData(d)$SampleID%>%paste(collapse = ",")})
+  dds<-paste("dds",1:length(dds),sep="")
+  results<-paste("res",1:length(res),sep="")
+  design<-des
+  df<-data.frame(dds=dds,results=results,design=design,samples=samples,filter_method=filter_method,n_filtered_genes=n_filtered_genes)
+  return(df)
+}
+
+
+if(dir.exists(deg.dir)){
+  a<-generate_DEG_input_summary_table(bronch.counts[,cols],ct,dds,res,deg.design)
+  write.csv(a,row.names=FALSE,file.path(deg.dir,paste("deg","bronch","continuous","analysis_input","cellcount_poscell+Batch12346",Sys.Date(),".csv",sep="_")))
+}
+
+## summary table of the DEG analysis
+generate_DEG_summary_table<-function(results_significant,deg_design,variable){
+  res.sig<-results_significant
+  deg.design<-deg_design
+  var<-variable
+  
+  reslist<-paste("res.sig",1:length(res.sig),sep="")
+  n_sig_deg<-unlist(sapply(res.sig,nrow))
+  design<-deg.design
+  
+  df<-data.frame(type="bronch",results=reslist,n_sig_deg=n_sig_deg,design=design,variable=var, row.names = NULL)
+  return(df)
+}
+
+if(dir.exists(deg.dir)){
+  a<-generate_DEG_summary_table(res.sig,deg.design,source.cell.log)
+  write.csv(a,row.names=FALSE,file.path(deg.dir,paste("dds","bronch","continuous","res_summary","cellcount_poscell+Batch12346",Sys.Date(),".csv",sep="_")))
+}
