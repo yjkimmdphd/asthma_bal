3897a5d33f2cbc5e55be75e49bfff252ec1dd444
yjkimmdphd <29801275+yjkimmdphd@users.noreply.github.com>
2024-08-24 21:58:22 -0400

    na

--- src/deg_analysis/deg_result_filtering.R
+++ src/deg_analysis/deg_result_filtering.R
@@ -2,6 +2,7 @@ library(dplyr)
 
 # set DEG results file folder 
 res_path<-file.path("./reports/temporary/deg_nasal_2024-08-24") 
+res_path<-file.path("./reports/temporary/deg_temporary_cont_ge0_2024-08-24")
 res_file_name<-list.files(res_path)
 res_index<-grep("res_sig",res_file_name)
 res_file<-file.path(res_path,res_file_name[res_index])
@@ -25,6 +26,9 @@ filtered_res_file_name<-paste("l2fc_mt1",res_file_name[res_index],sep="_")
 for(i in seq_along(filtered_res)){
   write.csv(filtered_res[[i]],file.path(res_path,filtered_res_file_name[i]))
 }
-  
+
+deg_sig_summary<-data.frame(analysis=res_file_name[res_index],n_deg_sig=sapply(filtered_res,nrow))
+deg_sig_summ_file_name<-"lfc_1_dds_nasal_continuous_all_and_dich_res_summary_cellcount+Batch12346_2024-08-24_.csv"
+write.csv(deg_sig_summary,file.path(res_path,deg_sig_summ_file_name))
 
 
