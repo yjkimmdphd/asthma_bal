de3dd27a2dcf1412ae8b828eaada8833b926a2da
yjkimmdphd <29801275+yjkimmdphd@users.noreply.github.com>
2025-02-14 15:44:18 -0500

    uses the original nasal biomarker and biomarker of asthma phenotype tables because the previous version was using an erroneous table, in which several rows of ACT score col data were shifted and caused artifactual differences

--- src/data_processing/making_phenotype_dataframe_with_batch_asthma_phenotype_cell_count.R
+++ src/data_processing/making_phenotype_dataframe_with_batch_asthma_phenotype_cell_count.R
@@ -4,13 +4,25 @@ library(dplyr)
 # 1) Define file paths
 # -----------------------------------------------------------------------------
 processed_data_folder <- "./resources/processed_data/"
-phenotype_file <- file.path(processed_data_folder, "yjk_Nasal_Biomarkers_BAL_2024-12-26.csv")
+phenotype_file_biomarker_proc <- file.path(processed_data_folder, "Nasal_Biomarkers_BAL_2025-02-01.csv")
+phenotype_file_biomarker_org <-file.path(processed_data_folder, "BiomarkersOfAsthma_original.csv")
 batch_info_file <- file.path(processed_data_folder, "nb_studyID_sampleID_batch.csv")
 
 # -----------------------------------------------------------------------------
 # 2) Read phenotype data
 # -----------------------------------------------------------------------------
-phenotype <- read.csv(phenotype_file)
+phenotype_biomarker_proc <- read.csv(phenotype_file_biomarker_proc)
+phenotype_biomarker_org <- read.csv(phenotype_file_biomarker_org)
+
+org_index<-which(phenotype_biomarker_org$ID%in%phenotype_biomarker_proc$ID)
+phenotype_biomarker_org<-phenotype_biomarker_org[org_index,]
+phenotype<-left_join(phenotype_biomarker_proc,phenotype_biomarker_org,by="ID")
+
+phenotype<-mutate(phenotype,bal_eos_p_mt1 = factor(case_when(BAL_eos_p > 1 ~ 1 , #"high_eos"
+                                                                           BAL_eos_p <= 1 ~ 0), # "low_eos"
+                              levels = c(0, 1)))
+
+t.test(ACT_score~bal_eos_p_mt1,data=phenotype)
 
 # -----------------------------------------------------------------------------
 # 3) Calculate BAL absolute eos and neut counts 
@@ -26,6 +38,9 @@ phenotype <- phenotype %>%
 # 4) Log-transform relevant columns
 # -----------------------------------------------------------------------------
 phenotype <- phenotype %>%
+  mutate(across(c(BAL_eos_ct, BAL_eos_p, BAL_wbc, BAL_neut_p, BAL_neut_ct, 
+                  blood_eos, blood_eos_p, blood_neut, blood_neut_p, blood_wbc), 
+                ~ as.numeric(as.character(.)))) %>%
   mutate(
     BAL_eos_ct_log   = log10(BAL_eos_ct + 0.001)   %>% round(2),
     BAL_eos_p_log    = log10(BAL_eos_p + 0.001)    %>% round(2), 
@@ -36,7 +51,7 @@ phenotype <- phenotype %>%
     blood_eos_p_log  = log10(blood_eos_p + 0.001)  %>% round(2),
     blood_neut_log   = log10(blood_neut + 0.001)   %>% round(2),
     blood_neut_p_log = log10(blood_neut_p + 0.001) %>% round(2),
-    blood_wbc_log    = log10(blood_wbc)            %>% round(2)
+    blood_wbc_log    = log10(blood_wbc + 0.001)    %>% round(2)  # Added 0.001 for consistency
   )
 
 # -----------------------------------------------------------------------------
@@ -84,7 +99,7 @@ phenotype <- rbind(n_phenotype, b_phenotype)
 # -----------------------------------------------------------------------------
 output_file <- file.path(
   processed_data_folder,
-  paste("scaled_phenotype_studyID_asthmaPhenotype_batch_cellCount_",today(),".csv",sep="")
+  paste("scaled_phenotype_studyID_asthmaPhenotype_batch_cellCount_",Sys.Date(),".csv",sep="")
 )
 
 write.csv(phenotype, output_file, row.names = FALSE)
